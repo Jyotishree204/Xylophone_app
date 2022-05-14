b76018245ccb38b5656bf79a8e9dfc8d6f00f440
Jyotishree204 <jyotishree204@gmail.com>
2022-05-14 16:14:12 +0530

    development

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,56 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   //const XylophoneApp({Key? key}) : super(key: key);
 
+  //int noteNumber
+  void playSound(int soundNumber) {
+    final player = AudioCache(prefix: 'assets/');
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({MaterialColor color, int soundNumber} ) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playSound(soundNumber);
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.grey,
+        // ),
         body: SafeArea(
           child: Center(
-            child: TextButton(child: Text('Click Me'), onPressed: () {
-              final player = AudioCache(prefix: 'assets/');
-              player.play('note1.wav');
-            },),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                buildKey(color: Colors.green, soundNumber: 4),
+                buildKey(color: Colors.blue, soundNumber: 5),
+                buildKey(color: Colors.indigo, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// child: TextButton(child: Text('Click Me'), onPressed: () {
+//               final player = AudioCache(prefix: 'assets/');
+//               player.play('note1.wav');
+//             },),
